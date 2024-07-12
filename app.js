0fb628803f6c554965e01a4e992efadf2988a15b
Victor Ukok <ukokjnr@gmail.com>
2024-07-12 14:57:45 +0100

    Fix: Modify mongodb connection url on app.js

--- app.js
+++ app.js
@@ -31,6 +31,9 @@ app.use("/v1/auth", authRouter);
 app.use('/v1/admin', adminRouter);
 app.use('/v1/users', usersRouter);
 
+app.get("/", (req, res) => {
+  res.status(200);
+});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
