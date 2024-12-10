9268e77e7497cb33722a87d9c4eddd0a581a020f
Victor <ukokjnr@gmail.com>
2024-12-10 05:07:09 +0100

    Form validation

--- routes/auth.js
+++ routes/auth.js
@@ -2,6 +2,7 @@ var express = require('express');
 const userModel = require('../models/usersModel');
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
+const Joi = require("joi");
 
 var router = express.Router();
 
@@ -10,6 +11,46 @@ router.post('/register', async function (req, res, next) {
   const { fullName, email, password } = req.body;
 
   console.log(req.body);
+
+  const {error} = Joi.object({
+    fullName: Joi.string().min(5).required().messages({
+      'string.base': 'Full name must be a string.',
+      'string.empty': 'Full name cannot be empty.',
+      'string.min': 'Full name must be at least 5 characters long.',
+      'any.required': 'Full name is required.'
+    }),
+    email: Joi.string().email({tlds: {allow: ["com", "net"]}}).required().messages({
+      'string.base': 'Email must be a string.',
+      'string.empty': 'Email cannot be empty.',
+      'string.email': 'Email must be a valid email address.',
+      'any.required': 'Email is required.'
+    }),
+    password: Joi.string().min(8).alphanum().required().messages({
+      'string.base': 'Password must be a string.',
+      'string.empty': 'Password cannot be empty.',
+      'string.min': 'Password must be at least 8 characters long.',
+      'string.alphanum': 'Password must be alphanumeric.',
+      'any.required': 'Password is required.'
+    })
+  }).validate({
+    fullName, email, password
+  });
+
+  if(error) {
+    res.status(400).send({
+      message: error.message
+    });
+    return;
+  }
+
+  const userAlreadyExist = await userModel.exists({email});
+
+  if(userAlreadyExist) {
+    res.status(409).send({
+      message: "User already exist"
+    });
+    return;
+  }
 
   const hashedPassword = bcrypt.hashSync(password, 10);
 
@@ -30,6 +71,29 @@ router.post('/login', async function (req, res, next) {
   // find, findOne, findById, findByIdAndUpdate, findOneAndUpdate
   // updateMany, deleteMany, countDocuments, findByIdAndDelete, findOneAndDelete,
   // distinct
+
+  const {error} = Joi.object({
+    email: Joi.string().email({tlds: {allow: ["com", "net"]}}).required().messages({
+      'string.base': 'Email must be a string.',
+      'string.empty': 'Email cannot be empty.',
+      'string.email': 'Email must be a valid email address.',
+      'any.required': 'Email is required.'
+    }),
+    password: Joi.string().min(8).alphanum().required().messages({
+      'string.base': 'Password must be a string.',
+      'string.empty': 'Password cannot be empty.',
+      'string.min': 'Password must be at least 8 characters long.',
+      'string.alphanum': 'Password must be alphanumeric.',
+      'any.required': 'Password is required.'
+    })
+  }).validate({email, password});
+
+  if(error) {
+    res.status(400).send({
+      message: error.message
+    });
+    return;
+  }
 
   const userDetails = await userModel.findOne({ email });
 
